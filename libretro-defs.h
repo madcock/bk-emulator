eccaa697d9f9f141a61646ffd15bf55241b1288d
Vladimir Serbinenko <phcoder@google.com>
2022-01-22 22:35:05 +0100

    LIBRETRO: Fix compilation for windows

--- libretro-defs.h
+++ libretro-defs.h
@@ -1,3 +1,5 @@
+#include "libretro.h"
+
 struct libretro_handle;
 struct libretro_handle *libretro_vfs_open(const char *filename, const char *mode);
 void libretro_vfs_close(struct libretro_handle *h);
@@ -5,7 +7,7 @@ int libretro_vfs_getc(struct libretro_handle *h);
 void libretro_vfs_putc(int c, struct libretro_handle *h);
 void libretro_vfs_flush(struct libretro_handle *h);
 int64_t libretro_vfs_get_size(struct libretro_handle *h);
-void libretro_vfs_read(struct libretro_handle *h, void *s, u_int64_t len);
+void libretro_vfs_read(struct libretro_handle *h, void *s, uint64_t len);
 void scr_mark_dirty (void);
 void tty_set_keymap (void);
 void tty_poll();
